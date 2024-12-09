c8085da5ebafc9691218dc61d6ce90753a1521f9
lanewells <wellsdelaneyk@gmail.com>
2024-12-09 15:46:36 -0800

    "Set up URL, view, and templates for my-memories."

--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Home Page</h1>')
+
+def my_memories(request):
+    return render(request, 'memories/my_memories.html')
