0ce98076bf5fbfc002edf7662ff36da4349d4d21
lanewells <wellsdelaneyk@gmail.com>
2024-12-09 16:07:10 -0800

    Set up URLs, views, and templates for profile, relatives, and family-memories pages.

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('relatives/', views.relatives, name='relatives'),
     path('my-memories/', views.my_memories, name='my-memories'),
+    path('family-memories/', views.family_memories, name='family-memories'),
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,5 +4,14 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Home Page</h1>')
 
+def profile(request):
+    return render(request, 'main/profile.html')
+
+def relatives(request):
+    return render(request, 'main/relatives.html')
+
 def my_memories(request):
     return render(request, 'memories/my_memories.html')
+
+def family_memories(request):
+    return render(request, 'memories/family-memories')
