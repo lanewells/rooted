87f3b5e09a31e0b4b961af112cd86acfa1c4ce68
lanewells <wellsdelaneyk@gmail.com>
2024-12-12 15:18:06 -0800

    Deployment attempt 3.

--- rootedapp/settings.py
+++ rootedapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('+Q68Q9\gf0*Qcn+ah^T')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not 'ON_HEROKU' in os.environ:
